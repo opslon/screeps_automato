6c957caece292e51c83f9bb87e17dc7690fa3578
Vitor de Almeida Chaves <valmeida@avaliaeducacional.com>
2020-03-26 21:47:09 -0300

    v0.1

--- src/main.js
+++ src/main.js
@@ -1,27 +1,25 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleTower = require('tower.atk');
+
 module.exports.loop = function () {
-    // This is the main game loop. The code inside of these curly brackets will run once per tick.
 
-    // if our creep doesn't exist, create it from our spawn
-    Game.spawns["Spawn1"].spawnCreep([WORK,CARRY,MOVE,MOVE], "My First Creep");
 
-    // get a reference to our creep
-    var mycreep = Game.creeps["My First Creep"];
 
-    // if creep has no energy, go to the energy source and harvest some
-    if (mycreep.store[RESOURCE_ENERGY] == 0) {
-        // make an easy reference to the energy source
-        var source = Game.getObjectById('16c3f93dd468ca9f065fd27c');
-        // move my creep to the energy source and harvest energy
-        mycreep.moveTo(source);
-        mycreep.harvest(source);
-    } else {
-        // but if our creep does have energy, bring it to the controller and upgrade it
-
-        // make an easy reference to the room's controller
-        var controller = mycreep.room.controller;
-        // move my creep to the controller and upgrade it
-        mycreep.moveTo(controller);
-        mycreep.upgradeController(controller);
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        if(tower.memory.role == 'towerAtk'){
+            roleTower.run(tower);
+        }
     }
-
 }
---
+++ src/tower.atk.js
@@ -0,0 +1,22 @@
+var roleTower = {
+    
+    run: function(tower) {
+    
+    
+  var tower = Game.getObjectById('d9e981017d86bc17ceada8d9');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+    }
+}
+module.exports = roleTower;
